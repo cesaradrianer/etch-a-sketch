9d5b3ae3f1c4cb6ee1c93abcfb7467de4f7ae6b5
César Adrián Reséndiz <cesaradrianer@gmail.com>
2023-11-19 23:51:38 -0600

    Modificaciones a index.js

--- index.js
+++ index.js
@@ -12,43 +12,45 @@ setGridsButton.onmouseup = () => {setGridsButton.style.backgroundColor = "#EBEBE
 grid.style.display = "grid"
 
 /*Getting the cells after creating them*/
-let grid_cells;
+let grid_cells
+
+let flag = false
 
 /*Inserting the grid cells*/
 set_grids_button.addEventListener('click', () => {
 
-    for (let i = 0; i < parseInt(setGridsInput.value); i++) {
+    for (let i = 0; i < Math.pow(parseInt(setGridsInput.value), 2); i++) {
 
         grid.insertAdjacentHTML("beforeend", "<div class='grid_cell'><div/>")
-        grid.style.gridTemplateColumns = `repeat(${parseInt(setGridsInput.value)}, ${setCellSize.value + "px"} 1fr)`
-        grid.style.gridTemplateRows = `repeat(${parseInt(setGridsInput.value)}, ${setCellSize.value + "px"} 1fr)`
 
+    }
+
+    grid.style.gridTemplateColumns = `repeat(${parseInt(setGridsInput.value)}, ${setCellSize.value + "px"} 1fr)`
+    grid.style.gridTemplateRows = `repeat(${parseInt(setGridsInput.value)}, ${setCellSize.value + "px"} 1fr)`
+
+    grid_cells = document.querySelectorAll(".grid_cell")
+
+    for (let j = 0; j < grid_cells.length; j++) {
+
+        grid_cells[j].style.backgroundColor = "#EBEBEB"
+    
+        grid_cells[j].addEventListener('mousedown', () => {
+            flag = true
+            grid_cells[j].style.backgroundColor = "#B0B0B0"
+        })
+    
+        grid_cells[j].addEventListener('mouseover', () => {
+            if(flag == true) {
+                grid_cells[j].style.backgroundColor = "#B0B0B0"
+            }
+        })
+    
+        grid_cells[j].addEventListener('mouseup', () => {
+            flag = false
+        })
+        
     }
 
 })
 
-grid_cells = document.querySelectorAll(".grid_cell")
-
-let flag = false
-
 /*Adding some events, so that when the mouse is down, one can start drawing in the grid*/
-for (let j = 0; j < grid_cells.length; j++) {
-
-    grid_cells[j].style.backgroundColor = "#EBEBEB"
-
-    grid_cells[j].addEventListener('mousedown', () => {
-        flag = true
-        grid_cells[j].style.backgroundColor = "#B0B0B0"
-    })
-
-    grid_cells[j].addEventListener('mouseover', () => {
-        if(flag == true) {
-            grid_cells[j].style.backgroundColor = "#B0B0B0"
-        }
-    })
-
-    grid_cells[j].addEventListener('mouseup', () => {
-        flag = false
-    })
-    
-}
